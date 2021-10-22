0efa59c40ca260ae014927f8f90c9cfa2d6d0bc4
Umer Malik <ibsoft0786@gmail.com>
2021-10-22 11:28:31 -0700

    App lunch

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\flutter_windows_1.22.4-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Return_0\Desktop\Grocery-App-master\Grocery-App-master\f_groceries"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
